8d4dc805f03c493ed45aff7901c366c32389ae85
Stiflerzak <hilarymwendia97@gmail.com>
2024-05-29 09:22:10 +0300

    First deployement

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Hilary's portfolio",
+  title: "Hilary Kariuki",
   description: "Modern and Minimalist Portfolio",
 };
 
